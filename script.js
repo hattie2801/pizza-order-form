4641545cf1382592acb2e2214e5fada65dc3f19a
hattie2801 <hattie2801@yahoo.com>
2025-05-05 19:47:25 -0500

    Updated script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Capture the Form Submission
+
+let button = document.getElementById("MyBut")
+
+button.addEventListener("click", displayMes)
+
+function displayMes() {
+    document.getElementById("but").innerHTML = "Order Complete";
+}
+
